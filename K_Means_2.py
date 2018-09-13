9210af800440499bc47240c3eb08c00ff5be2333
Anj32a <35238072+Rahan12345@users.noreply.github.com>
2018-09-13 22:50:19 +0530

    Update K_Means_2.py
    Comments added

--- K_Means_2.py
+++ K_Means_2.py
@@ -1,15 +1,20 @@
+# K-Means Clustering of Iris dataset using sklearn
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.cluster import KMeans
 
+# Loading iris dataset
 iris = datasets.load_iris()
 
+# Utilizing all data in iris dataset
 samples = iris.data
 
 model = KMeans(n_clusters=3)
 
+# Training model
 model.fit(samples)
 
+# Labelling
 labels = model.predict(samples)
 
 """print(labels)"""
@@ -18,6 +23,7 @@ labels = model.predict(samples)
 x = samples[:,0]
 y = samples[:,1]
 
+# Plotting the different labels
 plt.scatter(x,y,c=labels,alpha=0.5)
 
 plt.show()
